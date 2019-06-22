b0fa170a3a7411ef91885c6416f726ec91a9443d
Daniel Ang <dan.justin.ang@gmail.com>
2019-06-22 12:46:59 -0400

    Test commit - changed /Movies/Random to return Hello World

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -13,7 +13,8 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            return Content("Hello WOrld");
+           
         }
     }
 }
